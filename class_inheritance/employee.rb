92b3bceea15491683091d37e0c6f23697fe57728
“Frankelin <kelinp25@gmail.com>
2021-10-26 14:38:32 -0400

    employee done

--- class_inheritance/employee.rb
+++ class_inheritance/employee.rb
@@ -8,10 +8,45 @@ class Employee
     def bonus(multiplier)
         self.salary * multiplier
     end
+end
 
 
 
+class Manager < Employee
+    attr_reader :employees
+    def initialize(name, title, salary, boss)
+        super(name, title, salary, boss)
+        @employees = []
+    end
+
+    def bonus(multiplier)
+        total = 0
+        @employees.each do |employee|
+            total += employee.salary   
+        end
+
+        total * multiplier
+    end
+
+    def add_employee(person)
+        @employees << person if person.boss == self.name
+    end
 
 
 end
 
+ned = Employee.new("Ned", "Founder", 1_000_000, nil)
+darren = Manager.new("Darren", "TA Manager", 78_000, "Ned")
+shawna = Employee.new("Shawna", "TA", 12_000, "Darren")
+david = Employee.new("David", "TA", 10_000, "Darren")
+
+# p ned.bonus(5) # => 500_000
+# p darren.bonus(4) # => 88_000
+# p david.bonus(3) # => 30_000
+
+darren.add_employee(shawna)
+darren.add_employee(david)
+# p darren.employees
+puts
+
+p darren.bonus(4) # => 88_000
