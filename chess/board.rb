ce3417dd930be7d53f9c32cbb81abecc1e7579d8
Emily <myng.emily@gmail.com>
2021-10-27 15:15:44 -0400

    Creating piece class

--- chess/board.rb
+++ chess/board.rb
@@ -13,8 +13,8 @@ class Board
         @rows[0][0], @rows[0][7], @rows[7][0], @rows[7][7] = :R, :R, :R, :R #rook
         @rows[0][1], @rows[0][6], @rows[7][1], @rows[7][6] = :Kn, :Kn, :Kn, :Kn #knight
         @rows[0][2], @rows[0][5], @rows[7][2], @rows[7][5] = :B, :B, :B, :B, #bishop
-        @rows[0][3], @rows[7][3] = :Q, :Q #queen
-        @rows[0][4], @rows[7][4] = :K, :K #king
+        @rows[0][3], @rows[7][3] = :K, :K #king
+        @rows[0][4], @rows[7][4] = :Q, :Q #queen
     end
 
     def [](pos)
@@ -27,7 +27,7 @@ class Board
         @rows[row][col] = val
     end
 
-    def move_piece(color, start_pos, end_pos)
+    def move_piece(start_pos, end_pos) #needs color argument
         raise "error" if @rows[start_pos].empty? || !valid_pos?(end_pos)
 
         piece = @rows[start_pos]
--- chess/piece.rb
+++ chess/piece.rb
@@ -1,8 +1,30 @@
 class Piece
+    attr_reader :color, :board
+    attr_accessor :pos
+
     def initialize(color, baord, pos)
         @color = color
         @board = board
         @pos = pos
+    end
+
+    def to_s
+    end
+
+    def empty?
+        @board[@pos] != nil
+    end
+
+    def valid_moves
+    end
+
+    def pos=(val)
+    end
+
+    def symbol
+    end
+
+    def move_into_check?(end_pos)
     end
 end
 
